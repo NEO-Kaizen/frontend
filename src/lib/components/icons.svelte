7ad53f2c9013df5a06d735c7d5da07e6b1c9f272
Lucas Gomes <lucasg3984@gmail.com>
2026-08-05 13:16:55 -0300

    add material design icon component based on google fonts

---
+++ src/lib/components/icons.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { IconName, IconSize } from "$lib/types/icons";
+
+    // No futuro carregar apenas os icones que usamos
+    
+    interface Props{
+        iconName : IconName;
+        iconSize? : IconSize
+    }
+    
+    const {iconName, iconSize = 'md'}: Props = $props()
+    const iconDictionary = {
+        'Fila Centralizada': 'featured_play_list',
+        'Historico': 'history',
+        'Home': 'home',
+        "Gerenciar Usuarios": 'user_attributes'
+
+    } satisfies Record<IconName,string>
+    
+</script>
+
+<span class="material-symbols-outlined {iconSize}" aria-hidden="true">
+    {iconDictionary[iconName]}
+</span>
+
+<style>
+    /* Mudar quando tiver o design system*/
+    .sm {font-size: 16px;}
+    .md {font-size: 20px;}
+    .lg {font-size: 24px;}
+</style>
---
+++ src/lib/types/icons.ts
@@ -0,0 +1,9 @@
+export type IconName = 'Fila Centralizada' | 'Historico' | 'Home' | 'Gerenciar Usuarios'
+
+/**
+ * Icon sizes:
+ * - `sm`: 16px
+ * - `md`: 20px
+ * - `lg`: 24px
+ */
+export type IconSize = 'sm' | 'md' | 'lg';
