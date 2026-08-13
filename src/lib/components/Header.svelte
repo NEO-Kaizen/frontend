9728318d623f4692b4bc24a5b4b001705c10c771
Sabrina Souza <sabrinasouza.dev@gmail.com>
2026-08-13 08:18:55 -0300

    feat: add application routes and access protection

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -120,10 +120,12 @@
 					</div>
 				{/each}
 			</div>
-			<button class="nav-item" type="button">
-				<Icon iconName="logout" />
-				Sair
-			</button>
+			<form method="POST" action="/logout">
+				<button class="nav-item" type="submit">
+					<Icon iconName="logout" />
+					Sair
+				</button>
+			</form>
 		</div>
 	{/if}
 </header>
--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-    import LoginForm from '$lib/components/forms/LoginForm.svelte';
+	import LoginForm from '$lib/components/forms/LoginForm.svelte';
 </script>
-
 
 <main class="content-container">
 	<LoginForm />
