34c4fe901382fb05c384c086929543675eb217a5
Sabrina Souza <sabrinasouza.dev@gmail.com>
2026-08-13 10:02:11 -0300

    feat: implement login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
     import LoginForm from '$lib/components/forms/LoginForm.svelte';
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/layout/Footer.svelte';
 </script>
 
+<div class="login-page">
+    <Header />
 
-<main class="content-container">
-	<LoginForm />
-</main>
+    <main class="content-container">
+        <LoginForm />
+    </main>
+
+    <Footer />
+</div>
 
 <style>
-	main {
-		display: flex;
-		justify-content: center;
+    .login-page {
+		width: 100%;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
 		align-items: center;
-		height: 100vh;
-	}
+    }
+
+    .content-container {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
