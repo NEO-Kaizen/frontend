c2b3692ab16935637ba0260a8908eff2d660373e
Sabrina Souza <sabrinasouza.dev@gmail.com>
2026-08-13 20:40:05 -0300

    fix: adjust CSS and fix broken screen layout

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
     import LoginForm from '$lib/components/forms/LoginForm.svelte';
-    import Header from '$lib/components/Header.svelte';
 </script>
 
 <div class="login-page">
-    <Header />
-
     <main class="content-container">
         <LoginForm />
     </main>
@@ -14,7 +11,6 @@
 <style>
     .login-page {
 		width: 100%;
-        height: 100vh;
         max-height: calc(100vh - 40px);
         display: flex;
         flex-direction: column;
