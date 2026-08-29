c9b9ea46aa39495db1250001032ecbadd139a40d
SabrinaZ8 <86389273+SabrinaZ8@users.noreply.github.com>
2026-08-29 17:19:52 -0300

    Fix(header): remove margin and apply spacing in global layout (#69)
    * fix(header): remove margin and apply spacing in global layout
    
    * fix(header): adjust global layout spacing

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -153,7 +153,6 @@
 		display: flex;
 		flex-direction: column;
 		width: 90vw;
-		margin: 30px 80px;
 		gap: var(--spacing-md);
 		padding: var(--spacing-md) var(--spacing-lg);
 		background-color: var(--white);
